9f70ffd0139e3d04e45520e39e9024cf9459bd72
Ryan Murphy <TheDecoder007@gmail.com>
2022-08-25 17:20:54 -0400

    data file is now array, index maps over to create project cards

--- src/components/Projects/data.js
+++ src/components/Projects/data.js
@@ -1,7 +1,7 @@
-//when refactoring, turn this into an array of objects, and use map function 
+//when refactoring, turn this into an array of objects, and use map function
 //in the index file, instead of exporting/importing each one
-
-export const cardOneInfo = {
+export const projects = [
+  {
     title: "weTravel",
     description: "Javascript, Handlebars, Cloudinary, Sequelize, Express",
     line: "A travel blog site where users can login and create blogs, upload photos, and comment on other blogs.",
@@ -9,18 +9,19 @@ export const cardOneInfo = {
     alt: "Screenshots of the weTravel site",
     website: "https://murmuring-reef-03220.herokuapp.com/",
     repo: "https://github.com/TheDecoder007/weTravel",
-  }
-  export const cardTwoInfo = {
+  },
+  {
     title: "Decentraland",
     description: "Javascript, Node, Typescript, DCL SDK",
     line: "A Metaverse where building anything is possible, using the SDK. This link is to my personal project, where I test and implement techniques for advanced builds.",
     img: require("../../assets/photos/Voicebox.png"),
     alt: "Screenshots of Decentraland",
-    website: "https://play.decentraland.org/?position=142%2C106&realm=unicorn&island=I59kf",
+    website:
+      "https://play.decentraland.org/?position=142%2C106&realm=unicorn&island=I59kf",
     repo: "https://github.com/TheDecoder007/VoiceBox_Lawn",
-  }
+  },
 
-  export const cardThreeInfo = {
+  {
     title: "entertainMe",
     description: "Javascript, jQuery, Bulma",
     line: "An entertainment search site using IMDB API to pull movie info, image, and rating and displays as a card.",
@@ -28,9 +29,9 @@ export const cardOneInfo = {
     alt: "Screenshots of the entertainMe site",
     website: "https://vertis-z.github.io/entertain-me/",
     repo: "https://github.com/Vertis-Z/entertain-me",
-  }
+  },
 
-  export const cardFourInfo = {
+  {
     title: "Last Slice Collective",
     description: "Javascript, Node, Typescript, DCL SDK",
     line: "A web3 collective I've been working with, that onboards and builds experiences in the Metaverse for high-profile clients around the world.",
@@ -38,9 +39,9 @@ export const cardOneInfo = {
     alt: "Screenshots of the Last Slice site",
     website: "https://LastSlice.org",
     repo: "https://LastSlice.org",
-  }
+  },
 
-  export const cardFiveInfo = {
+  {
     title: "Weather Dashboard",
     description: "HTML, CSS, Javascript, Bulma, Weather API",
     line: "An app to get local weather reports. Current, 5 day, and recent serches saved.",
@@ -48,9 +49,8 @@ export const cardOneInfo = {
     alt: "Screenshots of the Weather Dashboard site",
     website: "https://thedecoder007.github.io/weather-dashboard/",
     repo: "https://github.com/TheDecoder007/weather-dashboard",
-  }
-  
-  export const cardSixInfo = {
+  },
+  {
     title: "My First Portfolio",
     description: "HTML, CSS",
     line: "My first portfolio page built after 4 weeks in class. How far I've come!",
@@ -58,9 +58,8 @@ export const cardOneInfo = {
     alt: "Screenshots of my first portfolio",
     website: "https://thedecoder007.github.io/challenge-two/",
     repo: "https://github.com/TheDecoder007/challenge-two",
-  }
-  
-  export const cardSevenInfo = {
+  },
+  {
     title: "Photo-port",
     description: "React, Javascript",
     line: "A class module using React to build a website that conditionally renders pages.",
@@ -68,9 +67,8 @@ export const cardOneInfo = {
     alt: "Screenshots of the Photo-port site",
     website: "https://github.com/TheDecoder007/photo-port",
     repo: "https://github.com/TheDecoder007/photo-port",
-  }
-
-  export const cardEightInfo = {
+  },
+  {
     title: "Quiz-Challenge",
     description: "HTML, CSS, Javascript",
     line: "A fun quiz to test your wits, with a timer, and button color displays right or wrong answers.",
@@ -78,4 +76,5 @@ export const cardOneInfo = {
     alt: "Screenshots of the quiz challenge",
     website: " https://github.com/TheDecoder007/quiz-challenge",
     repo: "https://thedecoder007.github.io/quiz-challenge/",
-  }
+  },
+];
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,33 +1,22 @@
 import React from "react";
 import ProjectCard from "./ProjectCard";
-import {
-  cardOneInfo,
-  cardTwoInfo,
-  cardThreeInfo,
-  cardFourInfo,
-  cardFiveInfo,
-  cardSixInfo,
-  cardSevenInfo,
-  cardEightInfo,
-} from "./data";
+import {projects} from "./data";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 
+
 function Projects() {
+
+
   return (
     <Container fluid className="projectCont" id="projects">
       <Row>
         <h3 className="text-center sectionHead">My Projects</h3>
       </Row>
       <Row className="CardRow">
-        <ProjectCard {...cardOneInfo} />
-        <ProjectCard {...cardTwoInfo} />
-        <ProjectCard {...cardThreeInfo} />
-        <ProjectCard {...cardFourInfo} />
-        <ProjectCard {...cardFiveInfo} />
-        <ProjectCard {...cardSixInfo} />
-        <ProjectCard {...cardSevenInfo} />
-        <ProjectCard {...cardEightInfo} />
+{projects.map(project => {
+  return <ProjectCard key={project.title} {...project}/>
+})}
       </Row>
     </Container>
   );
